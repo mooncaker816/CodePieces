88db09d9acf20092db7f0be8161b2228b6246b1e
Mingle <MingleGu@gmail.com>
2018-06-13 17:08:08 +0800

    add or-channel

--- patterns/or-channel_test.go
+++ patterns/or-channel_test.go
@@ -17,16 +17,16 @@ func TestOr(t *testing.T) {
 		randSig(r),
 		randSig(r),
 	)
-	fmt.Printf("closed after %v seconds!\n", time.Now().Sub(st))
+	fmt.Printf("closed after %v!\n", time.Since(st))
 }
 
 func randSig(r *rand.Rand) <-chan interface{} {
 	ch := make(chan interface{})
 	go func() {
 		defer close(ch)
-		sec := time.Duration(r.Int63n(10) + 3)
-		fmt.Printf("closing after %d seconds!\n", sec)
-		time.Sleep(sec * time.Second)
+		sec := time.Duration(r.Int63n(10)+3) * time.Second
+		fmt.Printf("closing after %v!\n", sec)
+		time.Sleep(sec)
 	}()
 	return ch
 }
